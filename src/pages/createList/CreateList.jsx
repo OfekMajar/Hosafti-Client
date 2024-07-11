e53d3d3a2129ddc8953503b94872abd34d653170
OfekMajar <ofek.majar@gmail.com>
2024-07-11 23:21:35 +0300

    [HOS-22] tried to use accessToken

--- src/pages/createList/CreateList.jsx
+++ src/pages/createList/CreateList.jsx
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { format } from 'date-fns';
 import heLocale from 'date-fns/locale/he'; // Import Hebrew locale
 import CreateListForm from '../../components/CreateListForm';
 import axios from 'axios';
 import { baseUrl } from '../../utils/backEndUtils';
 import { useNavigate, useParams } from 'react-router-dom';
+import { UserContext } from '../../context/User';
 
 function CreateList() {
   //Todo add validation so he can't make new list
   const { id } = useParams();
   const navigate = useNavigate();
   const [formData, setFormData] = useState({});
-
+  const { accessToken } = useContext(UserContext);
   const date = new Date();
   const formattedDate = date.toLocaleString('he-IL', {
     timeZone: 'Asia/Jerusalem',
@@ -20,7 +21,6 @@ function CreateList() {
     year: 'numeric',
   });
   console.log(formattedDate);
-
   const changeHandler = (e) => {
     formData[e.target.name] = e.target.value;
     setFormData({ ...formData });
@@ -36,7 +36,8 @@ function CreateList() {
           groupId: id,
           title: formData.title,
           date: { enDate: date, heDate: formattedDate },
-        }
+        },
+        { headers: { Authorization: `Bearer ${accessToken}` } }
       );
       const data = res.data;
       navigate(`/myGroups/group/${id}/groceryLists/groceryList/${data._id}`);
