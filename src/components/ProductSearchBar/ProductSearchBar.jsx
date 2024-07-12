d53d608bab57c99dbc6bd4c73e4f40bcae9859e3
OfekMajar <ofek.majar@gmail.com>
2024-07-13 00:40:46 +0300

    [HOS-17] Added filter for category

--- src/components/ProductSearchBar/ProductSearchBar.jsx
+++ src/components/ProductSearchBar/ProductSearchBar.jsx
@@ -1,17 +1,25 @@
-import axios from "axios";
-import React, { useState } from "react";
-import { baseUrl } from "../../utils/backEndUtils";
-import ProductSearchBarItem from "./ProductSearchBarItem";
-import styles from "./productSearchBar.module.css";
+import axios from 'axios';
+import React, { useState } from 'react';
+import { baseUrl } from '../../utils/backEndUtils';
+import ProductSearchBarItem from './ProductSearchBarItem';
+import styles from './productSearchBar.module.css';
+import { categories } from '../../utils/constains';
 function ProductSearchBar() {
   const [searchOptions, setSearchOptions] = useState([]);
+  const [category, setCategory] = useState('');
+
   const onChangeSearchBarHandler = async (e) => {
     const res = await axios.get(
-      `${baseUrl}/products/autoCompSearch?title=${e.target.value}`
+      `${baseUrl}/products/autoCompSearch?category=${category}&title=${e.target.value}`
     );
     const data = res.data;
     setSearchOptions(data);
   };
+
+  const handleCategorySelector = (e) => {
+    setCategory(e.target.value);
+  };
+
   return (
     <div className={styles.searchProductContainer}>
       <div className={styles.searchBar}>
@@ -20,6 +28,25 @@ function ProductSearchBar() {
           onChange={onChangeSearchBarHandler}
           placeholder="שם המוצר"
         />
+        <button
+          onClick={async () => {
+            let res = await axios.put(`${baseUrl}/products/normalize-titles`);
+            console.log(res);
+          }}
+        >
+          {' '}
+          fix
+        </button>
+        <select onChange={handleCategorySelector}>
+          <option value="">הכל</option>
+          {categories.map((category) => {
+            return (
+              <option key={`category-${category}`} value={category}>
+                {category}
+              </option>
+            );
+          })}
+        </select>
       </div>
       {searchOptions.length > 0 && (
         <div className={styles.searchOptionsContainer}>
---
+++ src/utils/constains.js
@@ -0,0 +1,1 @@
+export const categories = ['פירות', 'חלב וביצים', 'ירקות'];
